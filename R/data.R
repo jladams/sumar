ae9ef43803d0e767d1c9d6076a4e59f82f2ad88b
jladams <adamsjlp@gmail.com>
2016-08-08 11:44:38 -0400

    add defaults to api call

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-suma_from_api <- function(url, initiativeId, startDate, endDate) {
+suma_from_api <- function(url = "https://library.dartmouth.edu/suma/", initiativeId = 1, startDate = cut(Sys.Date(), "month"), endDate = Sys.Date()) {
   jsonlite::fromJSON(paste0(url, "analysis/reports/lib/php/rawDataResults.php?id=", initiativeId, "&sdate=", lubridate::ymd(startDate), "&edate=", lubridate::ymd(endDate)), flatten=TRUE) %>%
     dplyr::mutate(term=getTerms(time)) %>%
     dplyr::distinct(countId, .keep_all = TRUE)
