9ef119aee327cd5f5e3d74305da9dabd5dd29933
jladams <adamsjlp@gmail.com>
2016-08-15 14:00:37 -0400

    refine plot function

--- R/plot.R
+++ R/plot.R
@@ -4,12 +4,15 @@
 #' @param df Data frame containing Suma Data
 #' @param xVar What variable you'd like to use for the X axis of your plot(s)
 #' @param op What operation you want to use to get the data
-#' @param groupBy Optional variable defining how to group data
-#' @param filterBy Optional variable defining how to filter data
-#' @param facetBy Optional variable for faceting your plot
-#' @param fillBy Optional variable to define the fill of the bars
+#' @param groupBy Optional argument defining how to group data
+#' @param filterBy Optional argument defining how to filter data
+#' @param facetBy Optional argument to define faceting your plot
+#' @param fillBy Optional argument to define the fill of the bars
+#' @param yVar Optional argument to specify variable you'd like to use fo the Y axis of your plot(s), defaults to "value"
+#' @param legend Optional argument to decide whether to display legend (default TRUE)
+#' @param position Optional argument to define position of bars, takes standard ggplot2 geom_bar arguments
 #' @export
-suma_plot <- function(df, xVar, op = "max", groupBy = NULL, filterBy = NULL, facetBy = NULL, fillBy = NULL,  yVar = "value", legend = TRUE) {
+suma_plot <- function(df, xVar, op = "max", groupBy = NULL, filterBy = NULL, facetBy = NULL, fillBy = NULL,  yVar = "value", legend = TRUE, position = "stack") {
 
   # Decide which data function to use from op argument
   func <- match.fun(paste0("suma_", op, "_count"))
@@ -20,8 +23,8 @@ suma_plot <- function(df, xVar, op = "max", groupBy = NULL, filterBy = NULL, fac
   # Create plot using xVar and yVar (yVar defaults to "value")
   p <- ggplot2::ggplot(tmp, ggplot2::aes_string(x = xVar, y = yVar))
 
-  # Decide fill with fillBy
-  p <- p + ggplot2::geom_bar(ggplot2::aes_string(fill = fillBy), stat = "identity")
+  # Add bars
+  p <- p + ggplot2::geom_bar(ggplot2::aes_string(fill = fillBy), position = position, stat = "identity")
 
   # Remove legend if specified
   if(legend == FALSE) {
