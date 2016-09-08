4692303a0faa526a3445cb1f9f9246ae91dbf8b1
jladams <adamsjlp@gmail.com>
2016-09-08 17:45:33 -0400

    use query server for count data and dictionaries

--- R/test.R
+++ R/test.R
@@ -1,17 +1,18 @@
-tmp <- jsonlite::fromJSON("https://library.dartmouth.edu/sumaserver/query/sessions?id=1&format=CAL&sdate=20160901")
+tmp <- jsonlite::fromJSON("https://stats.lib.ou.edu/sumaserver/query/sessions?id=5&format=CAL&sdate=20160901")
 
+actGroups <- tmp$initiative$dictionary$activityGroups %>% unique() %>% dplyr::select(groupId = id, actGroup = title)
+locations <- tmp$initiative$dictionary$locations %>% unique() %>% dplyr::select(locId = id, location = title)
 activities <- tmp$initiative$dictionary$activities %>%
   unique() %>%
+  dplyr::select(actId = id, activity = title, groupId = activityGroup) %>%
   dplyr::left_join(actGroups)
 
-dplyr::select(activities = id, actName = title)
-actGroups <- tmp$initiative$dictionary$activityGroups %>% unique() %>% dplyr::select(groupId = id, actGroup = title)
-
-
 df <- tmp$initiative$sessions %>%
-  rename(sessionId = id) %>%
-  unnest() %>%
-  unnest()
+  dplyr::rename(sessionId = id) %>%
+  tidyr::unnest() %>%
+  tidyr::unnest() %>%
+  dplyr::rename(locId = location, actId = activities) %>%
+  left_join(locations) %>%
+  left_join(activities)
 
 colnames(df) <- c("sessionId", "sessionStart", "sessionEnd", "transStart", "transEnd", "countId", "time", "count", "location", "activities")
-df <-
